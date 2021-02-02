75d48384a568c5c22d9d9ed5c8303d11f41753d7
Nandaja <nandaja.varma@gmail.com>
2021-02-02 23:13:40 +0100

    Uploading the new resume

--- js/script.js
+++ js/script.js
@@ -4,19 +4,21 @@ $(".expandContent").click(function () {
 
     $header = $(this);
     //getting the next element
+    $icon = $(this).children('i').eq(0);
     $content = $header.next();
-    console.log($header.i);
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
         //change text of header based on visibility of content div
+
+
         if ($content.is(":visible")) {
-            $("i").removeClass("fa fa-chevron-circle-down");
-            $("i").addClass("fa fa-chevron-circle-up");
+            $icon.removeClass("fa fa-chevron-circle-down");
+            $icon.addClass("fa fa-chevron-circle-up");
         }
         else {
-            $("i").removeClass("fa fa-chevron-circle-up");
-            $("i").addClass("fa fa-chevron-circle-down");
+            $icon.removeClass("fa fa-chevron-circle-up");
+            $icon.addClass("fa fa-chevron-circle-down");
         }
         // $header.text(function () {
         //     //change text based on condition
